74271cfbd17e2d3dd645b7d670555d0ffb9c3b5a
akki007k <akashkgecse@gmail.com>
2016-12-18 01:01:44 +0530

    Add files via upload

---
+++ Assignment1_3.java
@@ -0,0 +1,20 @@
+class Assignment1_3 {
+	public static void main(String args[])
+	{
+		int x=10;
+		int y=20;
+		System.out.println("Before swaping the initial value of x is = "+x);
+		System.out.println("Before swaping the initial value of y is = "+y);
+	
+		x=x+y;
+		y=x-y;
+		x=x-y;
+		
+		System.out.println("After swaping the value of x is = "+x);
+		System.out.println("After swaping the value of y is = "+y);
+		
+		
+		
+	}
+
+}                   
---
+++ Assignment1_4.java
@@ -0,0 +1,13 @@
+class Assignment1_4 {
+	public static void main(String args[])
+	{
+		char x;
+		
+		for(int i=65;i<=90;i++)
+		{
+			x = (char)i;
+			System.out.println(i+"-"+x);
+		}
+	}
+
+}
